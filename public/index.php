fd2b17dbe9e9ad6c4e590f6b4cbc1de6e1837e26
james <yarbsemaj@gmail.com>
2019-04-23 02:43:00 +0100

    Added 404 for page not found

--- public/index.php
+++ public/index.php
@@ -7,5 +7,9 @@ include('../routes.php');
 
 $method = isset($_POST['_METHOD']) ? $_POST['_METHOD'] : $_SERVER['REQUEST_METHOD'];
 
+if (!isset($routes[$method . ':' . strtok($_SERVER['REQUEST_URI'], '?')])) {
+    (new \Views\Error(404))->render();
+}
+
 $routes[$method . ':' . strtok($_SERVER['REQUEST_URI'], '?')]->go();
 
