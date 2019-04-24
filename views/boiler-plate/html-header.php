8e9ec3148253941e1dc574d847ff141751f4e339
james <yarbsemaj@gmail.com>
2019-04-24 12:47:35 +0100

    Fixed error with user name display on navbar

--- views/boiler-plate/html-header.php
+++ views/boiler-plate/html-header.php
@@ -30,7 +30,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?php print (Auth::user()->first_name . " " . Auth::user()->surname) ?>
+                        <?php safePrint(Auth::user()->first_name . " " . Auth::user()->surname) ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="/logout">Logout</a>
